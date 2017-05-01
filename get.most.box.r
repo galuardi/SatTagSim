57c73bd3ea19dbf8c7c4ea8de5468412cad336c3
galuardi <drdrumfish@gmail.com>
2017-05-01 16:51:42 -0400

    changes to get.most.box; actually working now... does not seem to help anything.
    updates to vignette: added hard mask for summer GOMEX and winter GSL
    
    to do:

--- get.most.box.r
+++ get.most.box.r
@@ -1,25 +1,50 @@
-get.most.box <- function(simdatdf, syear=2000, boxes = box7, time.int = 'seqmonth', boxname = 'box', sim.len = 720){
+get.most.box <- function(simdatdf, syear=2000, boxes = box7,  boxname = 'box', sim.len = 720, step = c('day', 'Month','seas'), days = 30){
 
+  browser()
+
+  days = switch(step,
+                Month = 30,
+                seas = 90,
+                day = 1)
+
+  if(is.numeric(days)==F) stop('Need to define desired transition step (i.e., month, season, day)')
+
+  time.len = sim.len/days
   # do this for each tagid...
 
   n = sim.len
 
-  names(simdat) = 1:length(simdat)
-  datbox1 = get.box.vals(simdatdf[,2:4], boxes = boxes)
-  datbox1$TagID = as.vector(apply(data.frame(names(simdat)), 1, rep, n)) # 90 days in a season
-  years = syear:((syear+n)-1)
+  cidx = c(grep('lon', names(simdatdf)),grep('lat', names(simdatdf)),grep('Month', names(simdatdf)))
+
+
+  # names(simdat) = 1:length(simdat)
+  datbox1 = get.box.vals(simdatdf[,cidx], boxes = boxes)
+  datbox1$TagID = simdatdf[,1]
+  # datbox1$TagID = as.vector(apply(data.frame(names(simdat)), 1, rep, n)) # 90 days in a season
+  years = syear:((syear+n/360)-1)
+  yearvec = as.vector(apply(data.frame(years), 1, rep, sim.len/length(unique(years))))
+  datbox1$Year = rep(yearvec, length(unique(datbox1$TagID)))
   # sdat = datbox1[1:720,]
 
-  get.most.box.month = function(sdat, time.len = 24, days = 30, boxname = 'box'){
+  stepidx = grep(step, names(datbox1))
+
+  get.most.box.month = function(sdat, time.len = time.len, days = days, boxname = boxname){
     mseq = as.vector(sapply(1:time.len, function(i) rep(i, days)))
-    sdat$seqmonth = rep(mseq, length(unique(sdat$TagID))) # not good naming...
-    tagbox = as.numeric(attributes((daply(sdat, c('seqmonth', boxname), function(x) nrow(x))))$dimnames[[2]])
-    most.box = tagbox[apply(daply(sdat, c('seqmonth', boxname), function(x) nrow(x)),1,which.max)]
+    sdat$seqtime = rep(mseq, length(unique(sdat$TagID))) # not good naming...
+    tagbox = ldply(dlply(sdat, c('seqtime', boxname), function(x) nrow(x)))
+    names(tagbox)[3] = 'count'
+    most.box = ddply(tagbox, 'seqtime', function(x) data.frame(cbox = x$box[which.max(x$count)]))$cbox
+    # tagbox = attributes(dlply(sdat, c('seqtime', boxname), function(x) nrow(x)))$split_labels[,2]
+    # if(sd(tagbox, na.rm = T)==0){
+    #     most.box = 'you have a problem with this sim..'
+    # }else{
+    #  most.box = tagbox[apply(daply(sdat, c('seqtime', boxname), function(x) nrow(x)), 1, which.max)]
+    # }
     most.box
   }
 
   # simdatsub = ldply(simdat, function(x) x[seq(1, n, by = seas.len),])[,2:4]
-  simdatsub = ddply(datbox1, 'TagID', function(x) data.frame(Month = rep(unique(x$Month), 2), cbox = get.most.box.month(x)))
+  simdatsub = ddply(datbox1, 'TagID', function(x) data.frame(seqtime = rep(unique(x[,stepidx]), 2), cbox = get.most.box.month(sdat = x, time.len = time.len, boxname = boxname, days = days)))
 
   #
   # msimdat = melt(simdat, c('lon,','lat','Month'))
@@ -69,12 +94,14 @@ get.most.box <- function(simdatdf, syear=2000, boxes = box7, time.int = 'seqmont
     c(0,diff(x$cbox))
   }
 
-  flbox = ddply(datbox1, 'TagID', function(x) data.frame(x, btrans=get.btrans(x)))
+  flbox = ddply(simdatsub, 'TagID', function(x) data.frame(x, btrans=get.btrans(x)))
 
   flbox$pbox = flbox$cbox-flbox$btrans
 
-  flbox = flbox[,c('Year','season','TagID','pbox','btrans','cbox')]
+  flbox = flbox[,c('seqtime','TagID','pbox','btrans','cbox')]
 
   flbox
 }
+
+
 
