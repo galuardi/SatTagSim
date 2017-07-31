6965670a213c91b72699014a0ac4ab9d5f2a466c
galuardi <drdrumfish@gmail.com>
2017-07-31 16:42:48 -0400

    fixed get.start.pts for negative probability.. again

--- R/get.start.pts.r
+++ R/get.start.pts.r
@@ -22,6 +22,7 @@ get.start.pts <- function(dat, n = 100, months = 1:12, mask = rmask, posnames = 
 					  tmpr = make.sim.raster(tmp)
 					  tmpr[tmpr<0] = 0
 					  tmpr = raster::resample(tmpr, mask)*mask[[i]]
+					  tmpr[tmpr<0] = 0
 					  tmpdf = as.data.frame(as(tmpr, 'SpatialPointsDataFrame'))
 					  tmpdf[,1] = tmpdf[,1]/max(tmpdf[,1])
 					  pts = tmpdf[sample(1:nrow(tmpdf), n, prob = tmpdf[,1], replace = T),2:3] # note: the sampl
